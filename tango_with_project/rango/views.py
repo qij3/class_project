fb9345b06cb070efd72bfa20d73d86a058ae3ed1
mingyue <mingyue@mingyuedeMacBook-Air.local>
2015-02-14 22:26:09 -0800

    Index html

--- tango_with_project/rango/views.py
+++ tango_with_project/rango/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Rango says hey there world!")
+    context_dict = {'boldmessage': "I am bold font from the context"}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+
+    return render(request, 'rango/index.html', context_dict)
 
