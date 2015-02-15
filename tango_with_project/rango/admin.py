865fc3fac074bf6ecbda4d7402258b5d2cdbcd46
mingyue <mingyue@mingyuedeMacBook-Air.local>
2015-02-14 23:39:10 -0800

    chapter 7

--- tango_with_project/rango/admin.py
+++ tango_with_project/rango/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 from rango.models import Category, Page
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
 
-admin.site.register(Category)
+# Update the registeration to include this customised interface
+admin.site.register(Category, CategoryAdmin)
+
 admin.site.register(Page)
 
--- tango_with_project/rango/views.py
+++ tango_with_project/rango/views.py
@@ -3,9 +3,42 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render
+from rango.models import Category
+from rango.models import Page
+
+def category(request, category_name_slug):
+
+    # Create a context dictionary which we can pass to the template rendering engine.
+    context_dict = {}
+
+    try:
+        # Can we find a category name slug with the given name?
+        # If we can't, the .get() method raises a DoesNotExist exception.
+        # So the .get() method returns one model instance or raises an exception.
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+
+        # Retrieve all of the associated pages.
+        # Note that filter returns >= 1 model instance.
+        pages = Page.objects.filter(category=category)
+
+        # Adds our results list to the template context under name pages.
+        context_dict['pages'] = pages
+        # We also add the category object from the database to the context dictionary.
+        # We'll use this in the template to verify that the category exists.
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        # We get here if we didn't find the specified category.
+        # Don't do anything - the template displays the "no category" message for us.
+        pass
+
+    # Go render the response and return it to the client.
+    return render(request, 'rango/category.html', context_dict)
+
 
 def index(request):
-    context_dict = {'boldmessage': "I am bold font from the context"}
+    category_list = Category.objects.all()[:5]
+    context_dict = {'categories': category_list}
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
